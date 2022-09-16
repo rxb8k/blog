e172fcb6c51feaa30a282e68ed7cd6421b0a4fce
Jinseo Song <82093280+rxb8k@users.noreply.github.com>
2022-09-17 01:03:57 +0900

    docs: projectsData 틀 수정

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -1,26 +1,26 @@
 const projectsData = [
   {
     title: '더꾸',
-    description: '설명',
-    imgSrc: '/static/images/google.png',
-    href: 'https://www.google.com',
+    description: '토큰형 앨범 및 온라인 앨범 구매·보관 플랫폼',
+    imgSrc: '/static/images/hackathon/1.png',
+    href: '/blog/project/likelion-10th-hackaton-review',
   },
   {
     title: 'BLOV',
-    description: 'ㅇㅇ',
-    imgSrc: '/static/images/google.png',
+    description: 'NFT 기반 전자헌혈증 및 헌혈증 전달 플랫폼',
+    imgSrc: '/static/images/superchallenge/blov.png',
     href: '/blog/the-time-machine',
   },
   {
-    title: '플젝 3',
-    description: 'ㅇㅇ',
-    imgSrc: '/static/images/google.png',
+    title: 'BERT를 활용한 PostgreSQL의 Query Planner 성능 개선 프로젝트',
+    description: '',
+    imgSrc: '/static/images/graduation-project/service.png',
     href: '/blog/the-time-machine',
   },
   {
-    title: '플젝 4',
-    description: 'ㅇㅇ',
-    imgSrc: '/static/images/google.png',
+    title: '스마트 쓰레기통 디바이스 및 관리자용 플랫폼 개발',
+    description: '',
+    imgSrc: '/static/images/vip-project/1.png',
     href: '/blog/the-time-machine',
   },
 ]
